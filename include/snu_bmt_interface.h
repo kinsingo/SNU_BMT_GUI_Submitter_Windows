f68f5f39bffccf1e1e5825e5213ca61560e0dac9
jonghyun shin <jonghyun_shin@naver.com>
2024-09-13 13:14:06 +0900

    240913 Interfaced Modified

--- include/snu_bmt_interface.h
+++ include/snu_bmt_interface.h
@@ -10,13 +10,27 @@
 #include <iostream>
 using namespace std;
 
+// class EXPORT_SYMBOL SNU_BMT_Interface
+// {
+// public:
+//    virtual ~SNU_BMT_Interface(){}
+//    virtual bool requiresModelConversion() = 0;
+//    virtual string convertModel(const string& model) = 0;
+//    virtual string runInference(const string& model, const string& data) = 0;
+// };
+
+//240913
+//ONNX Model은 Web을 통해 사용자가 직접 다운로드하도록 하고,
+//NPU에 맞게 컴파일된 Model을 Load 하는 작업은 main 함수에서 실행하며,
+//Load된 Model을 멤버 변수로 저장하여, runInference 함수에서 이를 사용하도록 권장함.
+//이렇게 하면 runInference 함수에 Model Loading 시간이 포함되지 않도록 할 수 있음.
 class EXPORT_SYMBOL SNU_BMT_Interface
 {
 public:
-    virtual ~SNU_BMT_Interface(){}
-    virtual bool requiresModelConversion() = 0;
-    virtual string convertModel(const string& model) = 0;
-    virtual string runInference(const string& model, const string& data) = 0;
+   virtual ~SNU_BMT_Interface(){}
+   virtual string runInference(const string& data) = 0;
 };
 
 #endif // SNU_BMT_INTERFACE_H
+
+
--- main.cpp
+++ main.cpp
@@ -10,27 +10,27 @@ using namespace std;
 class Virtual_Submitter_Implementation : public SNU_BMT_Interface
 {
 private:
-    void SleepForRandomMilliSeconds(const int& Min_ms, const int& Max_ms)
-    {
-        random_device rd;
-        mt19937 gen(rd());
-        uniform_int_distribution<> distr(Min_ms, Max_ms); // 5ms ~ 50ms 사이의 랜덤 값
-        int delay = distr(gen); // 랜덤 지연 시간을 생성합니다.
-        this_thread::sleep_for(chrono::milliseconds(delay)); // 지연 시간을 적용합니다.
-    }
-
+    string model;
+    string LoadModel()
+        {
+            return "DeepX NPU model";
+        }
 public:
-    bool requiresModelConversion() override
+    Virtual_Submitter_Implementation() 
     {
-        cout << "requiresModelConversion() is called from a DeepX NPU submitter" <<endl;
-        return true;
+        model = LoadModel();
     }
-    string convertModel(const string &model) override
-    {
-        cout << "convertModel(model) is called from a DeepX NPU submitter" <<endl;
-        return "Converted(" + model + ") from a DeepX NPU submitter";
-    }
-    string runInference(const string &model, const string &data) override
+private:
+    void SleepForRandomMilliSeconds(const int& Min_ms, const int& Max_ms)
+        {
+            random_device rd;
+            mt19937 gen(rd());
+            uniform_int_distribution<> distr(Min_ms, Max_ms); // 5ms ~ 50ms 사이의 랜덤 값
+            int delay = distr(gen); // 랜덤 지연 시간을 생성합니다.
+            this_thread::sleep_for(chrono::milliseconds(delay)); // 지연 시간을 적용합니다.
+        }
+public:
+    string runInference(const string &data) override
     {
         cout << "runInference(model,data) is called from a DeepX NPU submitter" <<endl;
         const int Min_ms = 5;
@@ -41,8 +41,10 @@ public:
 };
 
 int main(int argc, char *argv[])
-{
+{    
     shared_ptr<SNU_BMT_Interface> interface = make_shared<Virtual_Submitter_Implementation>();
     SNU_BMT_GUI_Caller caller(interface);
     return caller.call_BMT_GUI(argc,argv);
 }
+
+
